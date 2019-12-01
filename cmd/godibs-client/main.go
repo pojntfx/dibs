9625e01a1c02f73e421df8491015de9523fe0b14
Felix Pojtinger <felix@pojtinger.com>
2019-12-01 14:07:41 +0100

    refactor: Start decomposing client functions

--- cmd/godibs-client/main.go
+++ cmd/godibs-client/main.go
@@ -1,34 +1,20 @@
 package main
 
 import (
-	"errors"
-	fswatch "github.com/andreaskoch/go-fswatch"
-	"github.com/go-redis/redis/v7"
-	"github.com/otiai10/copy"
 	"github.com/pojntfx/godibs/pkg/config"
 	"github.com/pojntfx/godibs/pkg/utils"
-	rz "gitlab.com/z0mbie42/rz-go/v2"
 	"gitlab.com/z0mbie42/rz-go/v2/log"
-	git "gopkg.in/src-d/go-git.v4"
-	gitconf "gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/signal"
-	"path/filepath"
-	"strconv"
-	"strings"
 	"syscall"
-	"time"
 )
 
 // TODO:
-// - Decompose all functions
 // - Use struct for pipeline and the pipeline only (only define it once, then use the `.Run()` function twice)
 
 func main() {
-	err, m := GetModuleName(config.SYNC_MODULE_PUSH_MOD_FILE)
+	err, m := utils.GetModuleName(config.SYNC_MODULE_PUSH_MOD_FILE)
 	if err != nil {
 		panic(err)
 	}
@@ -36,24 +22,24 @@ func main() {
 	r := utils.NewRedisClient(config.REDIS_URL)
 
 	log.Info("Registering module ...")
-	RegisterModule(r, config.REDIS_CHANNEL_PREFIX, m)
+	utils.RegisterModule(r, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_REGISTERED, m)
 
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
 		log.Info("Unregistering module ...")
-		UnregisterModule(r, config.REDIS_CHANNEL_PREFIX, m)
+		utils.UnregisterModule(r, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_UNREGISTERED, m)
 		os.Exit(0)
 	}()
 
-	w := GetNewFolderWatcher(config.SYNC_MODULE_PUSH_WATCH_GLOB, config.PUSH_DIR)
+	w := utils.GetNewFolderWatcher(config.SYNC_MODULE_PUSH_WATCH_GLOB, config.PUSH_DIR)
 
 	first := make(chan struct{}, 1)
 	first <- struct{}{}
 	var commandStartState *exec.Cmd
 
-	err = RunPipeline(r, m, commandStartState, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_BUILT, config.REDIS_CHANNEL_MODULE_TESTED, config.REDIS_CHANNEL_MODULE_PUSHED, config.REDIS_CHANNEL_MODULE_STARTED, config.COMMAND_BUILD, config.COMMAND_TEST, config.COMMAND_START, config.GIT_BASE_URL, config.GIT_REMOTE_NAME, config.GIT_NAME, config.GIT_EMAIL, config.SRC_DIR, config.PUSH_DIR)
+	err = utils.RunPipeline(r, m, commandStartState, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_BUILT, config.REDIS_CHANNEL_MODULE_TESTED, config.REDIS_CHANNEL_MODULE_PUSHED, config.REDIS_CHANNEL_MODULE_STARTED, config.COMMAND_BUILD, config.COMMAND_TEST, config.COMMAND_START, config.GIT_BASE_URL, config.GIT_REMOTE_NAME, config.GIT_NAME, config.GIT_EMAIL, config.GIT_COMMIT_MESSAGE, config.SRC_DIR, config.PUSH_DIR)
 	if err != nil {
 		panic(err)
 	}
@@ -62,185 +48,11 @@ func main() {
 		select {
 		case <-first:
 		case <-w.ChangeDetails():
-			err := RunPipeline(r, m, commandStartState, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_BUILT, config.REDIS_CHANNEL_MODULE_TESTED, config.REDIS_CHANNEL_MODULE_PUSHED, config.REDIS_CHANNEL_MODULE_STARTED, config.COMMAND_BUILD, config.COMMAND_TEST, config.COMMAND_START, config.GIT_BASE_URL, config.GIT_REMOTE_NAME, config.GIT_NAME, config.GIT_EMAIL, config.SRC_DIR, config.PUSH_DIR)
+			err := utils.RunPipeline(r, m, commandStartState, config.REDIS_CHANNEL_PREFIX, config.REDIS_CHANNEL_MODULE_BUILT, config.REDIS_CHANNEL_MODULE_TESTED, config.REDIS_CHANNEL_MODULE_PUSHED, config.REDIS_CHANNEL_MODULE_STARTED, config.COMMAND_BUILD, config.COMMAND_TEST, config.COMMAND_START, config.GIT_BASE_URL, config.GIT_REMOTE_NAME, config.GIT_NAME, config.GIT_EMAIL, config.GIT_COMMIT_MESSAGE, config.SRC_DIR, config.PUSH_DIR)
 
 			if err != nil {
 				panic(err)
 			}
 		}
 	}
-}
-
-// RunPipeline runs the entire pipeline
-func RunPipeline(r *redis.Client, m string, commandStartState *exec.Cmd, channelPrefix, moduleBuildSuffix, moduleTestedSuffix, modulePushedSuffix, moduleStartedSuffix, commandBuild, commandTest, commandStart, gitBaseUrl, gitRemoteName, gitName, gitEmail, srcDir, pushDir string) error {
-	log.Info("Stopping module ...")
-	if commandStartState != nil {
-		commandStartState.Process.Kill()
-	}
-
-	log.Info("Copying module ...")
-	err := SetupPushDir(srcDir, pushDir)
-	if err != nil {
-		return err
-	}
-
-	log.Info("Building module ...")
-	err = RunCommand(r, channelPrefix, moduleTestedSuffix, m, commandBuild, false)
-	if err != nil {
-		return err
-	}
-
-	log.Info("Testing module ...")
-	err = RunCommand(r, channelPrefix, moduleTestedSuffix, m, commandTest, false)
-	if err != nil {
-		return err
-	}
-
-	log.Info("Pushing module ...")
-	pushURL := GetGitURL(gitBaseUrl, m)
-	err = PushModule(r, channelPrefix, modulePushedSuffix, m, pushDir, gitRemoteName, pushURL, gitName, gitEmail)
-	if err != nil {
-		return err
-	}
-
-	log.Info("Starting module ...")
-	err = RunCommand(r, channelPrefix, moduleStartedSuffix, m, commandStart, true)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// GetGitURL returns the URL of a git repo
-func GetGitURL(baseURL, m string) string {
-	return baseURL + "/" + m
-}
-
-// GetModuleName returns the module name from `go.mod`
-func GetModuleName(goModFilePath string) (error, string) {
-	f, err := ioutil.ReadFile(goModFilePath)
-	if err != nil {
-		return errors.New("Could not read module file"), ""
-	}
-
-	for _, line := range strings.Split(string(f), "\n") {
-		if strings.Contains(line, "module") {
-			return nil, strings.Split(line, "module ")[1]
-		}
-	}
-
-	return errors.New("Could find module declaration"), ""
-}
-
-// GetNewFolderWatcher returns a new folder watcher
-func GetNewFolderWatcher(watchGlob, pushDir string) *fswatch.FolderWatcher {
-	w := fswatch.NewFolderWatcher(watchGlob, true, func(path string) bool { return strings.Contains(path, pushDir) }, 1)
-	w.Start()
-
-	return w
-}
-
-// RegisterModule registers a module in Redis
-func RegisterModule(r *redis.Client, prefix, m string) {
-	r.Publish(prefix+":"+config.REDIS_CHANNEL_MODULE_REGISTERED, withTimestamp(m))
-}
-
-// UnregisterModule unregisters a module from Redis
-func UnregisterModule(r *redis.Client, prefix, m string) {
-	r.Publish(prefix+":"+config.REDIS_CHANNEL_MODULE_UNREGISTERED, withTimestamp(m))
-}
-
-// RunCommand runs or starts a command creates a corresponding message in Redis
-func RunCommand(r *redis.Client, prefix, suffix, m, command string, start bool) error {
-	c := exec.Command(strings.Split(command, " ")[0], strings.Split(command, " ")[1:]...)
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	var err error
-	if start {
-		err = c.Start()
-	} else {
-		err = c.Run()
-	}
-	if err != nil {
-		return err
-	}
-	r.Publish(prefix+":"+suffix, withTimestamp(m))
-	return nil
-}
-
-// SetupPushDir creates a temporary directory to do the git operations in
-func SetupPushDir(srcDir, pushDir string) error {
-	err := os.RemoveAll(pushDir)
-	if err != nil {
-		return err
-	}
-
-	err = os.MkdirAll(pushDir, 0777)
-	if err != nil {
-		return err
-	}
-
-	log.Info("Copying internal", rz.String("src", srcDir), rz.String("dist", pushDir))
-	err = copy.Copy(srcDir, pushDir)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// PushModule adds all files to a git repo, commits and finally pushes them to a remote
-func PushModule(r *redis.Client, prefix, suffix, m, pushDir, gitRemoteName, pushURL, gitName, gitEmail string) error {
-	g, err := git.PlainOpen(filepath.Join(pushDir))
-	if err != nil {
-		return err
-	}
-
-	_, err = g.CreateRemote(&gitconf.RemoteConfig{
-		Name: gitRemoteName,
-		URLs: []string{pushURL},
-	})
-	if err != nil {
-		return err
-	}
-
-	wt, err := g.Worktree()
-	if err != nil {
-		return err
-	}
-
-	_, err = wt.Add(".")
-	if err != nil {
-		return err
-	}
-
-	_, err = wt.Commit(withTimestamp(config.GIT_COMMIT_MESSAGE), &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  gitName,
-			Email: gitEmail,
-			When:  time.Now(),
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	err = g.Push(&git.PushOptions{
-		RemoteName: gitRemoteName,
-		RefSpecs:   []gitconf.RefSpec{"+refs/heads/master:refs/heads/master"},
-	})
-	if err != nil {
-		return err
-	}
-
-	r.Publish(prefix+":"+suffix, withTimestamp(m))
-
-	return nil
-}
-
-// withTimestamp gets a message name with the current timestamp
-func withTimestamp(m string) string {
-	t := time.Now().UnixNano()
-	return m + "@" + strconv.Itoa(int(t))
 }
--- pkg/utils/module.go
+++ pkg/utils/module.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"errors"
+	"github.com/go-redis/redis/v7"
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 )
@@ -18,4 +21,30 @@ func GetPathForModule(baseDir, m string) string {
 	fullModulePath := filepath.Join(pathParts...)
 
 	return fullModulePath
+}
+
+// GetModuleName returns the module name from `go.mod`
+func GetModuleName(goModFilePath string) (error, string) {
+	f, err := ioutil.ReadFile(goModFilePath)
+	if err != nil {
+		return errors.New("Could not read module file"), ""
+	}
+
+	for _, line := range strings.Split(string(f), "\n") {
+		if strings.Contains(line, "module") {
+			return nil, strings.Split(line, "module ")[1]
+		}
+	}
+
+	return errors.New("Could find module declaration"), ""
+}
+
+// RegisterModule registers a module in Redis
+func RegisterModule(r *redis.Client, prefix, suffix, m string) {
+	r.Publish(prefix+":"+suffix, WithTimestamp(m))
+}
+
+// UnregisterModule unregisters a module from Redis
+func UnregisterModule(r *redis.Client, prefix, suffix, m string) {
+	r.Publish(prefix+":"+suffix, WithTimestamp(m))
 }
