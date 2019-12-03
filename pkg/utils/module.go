1354c7374b009024b0d9895514b57d112009b6f6
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-03 17:43:14 +0100

    feat: Add replace directives in GetModuleWithReplaces

--- pkg/utils/module.go
+++ pkg/utils/module.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"errors"
-	"io/ioutil"
 	"path/filepath"
 	"strings"
 )
@@ -24,17 +23,64 @@ func GetPathForModule(baseDir, module string) string {
 }
 
 // GetModuleName returns the module name from `go.mod`
-func GetModuleName(goModFilePath string) (error, string) {
-	f, err := ioutil.ReadFile(goModFilePath)
-	if err != nil {
-		return errors.New("Could not read module file"), ""
-	}
-
-	for _, line := range strings.Split(string(f), "\n") {
+func GetModuleName(content string) (error, string) {
+	for _, line := range strings.Split(string(content), "\n") {
 		if strings.Contains(line, "module") {
 			return nil, strings.Split(line, "module ")[1]
 		}
 	}
 
 	return errors.New("Could find module declaration"), ""
+}
+
+// GetModuleWithReplaces returns the module file content with additional replacement declarations
+func GetModuleWithReplaces(content string, modulesToReplace []string, hostReplacement string) string {
+	var requires []string
+	var isInRequiresBlock bool
+
+	for _, line := range strings.Split(content, "\n") {
+		if strings.Contains(line, "require (") {
+			isInRequiresBlock = true
+			continue
+		}
+
+		if strings.Contains(line, ")") {
+			isInRequiresBlock = false
+			continue
+		}
+
+		if isInRequiresBlock {
+			lineParts := strings.Split(line, " ")
+			requireWithoutVersion := strings.TrimSpace(strings.Join(lineParts[:len(lineParts)-1], ""))
+			requires = append(requires, requireWithoutVersion)
+		}
+	}
+
+	var replaces []string
+
+	for _, require := range requires {
+		for _, moduleToReplace := range modulesToReplace {
+			if require == moduleToReplace {
+				moduleSuffixes := strings.Split(require, "/")[1:]
+				modulePartsWithHostReplace := append([]string{hostReplacement}, moduleSuffixes...)
+				moduleWithReplacedHost := strings.Join(modulePartsWithHostReplace, "/")
+
+				replaces = append(replaces, moduleWithReplacedHost)
+			}
+		}
+	}
+
+	var replaceBlock string
+
+	replaceBlock = replaceBlock + "\n// GODIBS:TEMPREPLACE:START"
+
+	for index, replace := range replaces {
+		moduleWithReplacePrefix := "replace " + requires[index] + " => " + replace + " master"
+
+		replaceBlock = replaceBlock + "\n" + moduleWithReplacePrefix
+	}
+
+	replaceBlock = replaceBlock + "\n// GODIBS:TEMPREPLACE:END"
+
+	return content + "\n" + replaceBlock
 }
