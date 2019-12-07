e98bfe92da6e3043913ea291b9ae35c9a8e3e1db
Felix Pojtinger <felix@pojtinger.com>
2019-12-07 12:47:33 +0100

    feat: Create starter for client

---
+++ pkg/starters/client.go
@@ -0,0 +1,189 @@
+package starters
+
+import (
+	"github.com/pojntfx/godibs/pkg/utils"
+	"github.com/pojntfx/godibs/pkg/workers"
+	rz "gitlab.com/z0mbie42/rz-go/v2"
+	"gitlab.com/z0mbie42/rz-go/v2/log"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+type Client struct {
+	PipelineUpFileMod         string
+	PipelineDownModules       string
+	PipelineDownDirModules    string
+	RedisUrl                  string
+	RedisPrefix               string
+	RedisSuffixUpRegistered   string
+	RedisSuffixUpUnRegistered string
+	GitUpRemoteName           string
+	GitUpBaseURL              string
+	GitUpUserName             string
+	GitUpUserEmail            string
+	GitUpCommitMessage        string
+	PipelineUpTestCommand     string
+	RedisSuffixUpTested       string
+	PipelineUpBuildCommand    string
+	RedisSuffixUpBuilt        string
+	PipelineUpStartCommand    string
+	RedisSuffixUpStarted      string
+	RedisSuffixUpPushed       string
+	PipelineUpDirSrc          string
+	PipelineUpDirPush         string
+	PipelineUpDirWatch        string
+	PipelineUpRegexIgnore     string
+}
+
+func (client *Client) Start() {
+	// Get the name of the module that is to be pushed
+	rawGoModContent, err := ioutil.ReadFile(client.PipelineUpFileMod)
+	if err != nil {
+		log.Fatal("Error", rz.String("System", "Client"), rz.Err(err))
+	}
+	// Get the content of the Go module file
+	goModContent := string(rawGoModContent)
+	err, module := utils.GetModuleName(goModContent)
+	if err != nil {
+		log.Fatal("Error", rz.String("System", "Client"), rz.Err(err), rz.String("Module", module))
+	}
+	// Get the modules that are to be downloaded
+	downModules := utils.GetModulesFromRawInputString(client.PipelineDownModules)
+	// Directory to clone the local modules to
+	downModulesDir := client.PipelineDownDirModules
+
+	// Replace the modules that are specified
+	moduleWithReplaces := utils.GetModuleWithReplaces(goModContent, downModules, downModulesDir)
+	ioutil.WriteFile(client.PipelineUpFileMod, []byte(moduleWithReplaces), 0777)
+
+	// Connect to Redis
+	redis := utils.Redis{
+		Addr:   client.RedisUrl,
+		Prefix: client.RedisPrefix,
+	}
+	redis.Connect()
+
+	// Register the module
+	log.Info("Registering module ...", rz.String("Module", module))
+	redis.PublishWithTimestamp(client.RedisSuffixUpRegistered, module)
+
+	// Unregister the module on interrupt signal
+	interrupt := make(chan os.Signal, 2)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-interrupt
+
+		log.Info("Unregistering module ...", rz.String("Module", module))
+		redis.PublishWithTimestamp(client.RedisSuffixUpUnRegistered, module)
+
+		log.Info("Cleaning up ...", rz.String("Module", module), rz.String("ModuleFile", client.PipelineUpFileMod))
+		rawGoModContent, err := ioutil.ReadFile(client.PipelineUpFileMod)
+		if err != nil {
+			log.Fatal("Error", rz.String("System", "Client"), rz.Err(err))
+		}
+		goModContent := string(rawGoModContent)
+		moduleWithoutReplaces := utils.GetModuleWithoutReplaces(goModContent)
+		ioutil.WriteFile(client.PipelineUpFileMod, []byte(moduleWithoutReplaces), 0777)
+
+		os.Exit(0)
+	}()
+
+	// Setup the pipeline
+	var commandStartState *exec.Cmd
+
+	git := utils.Git{
+		RemoteName:    client.GitUpRemoteName,
+		RemoteURL:     utils.GetGitURL(client.GitUpBaseURL, module),
+		UserName:      client.GitUpUserName,
+		UserEmail:     client.GitUpUserEmail,
+		CommitMessage: client.GitUpCommitMessage,
+	}
+
+	testCommand, buildCommand, startCommand := utils.EventedCommand{
+		LogMessage:   "Running test command ...",
+		ExecLine:     client.PipelineUpTestCommand,
+		RedisSuffix:  client.RedisSuffixUpTested,
+		RedisMessage: module,
+	}, utils.EventedCommand{
+		LogMessage:   "Running build command ...",
+		ExecLine:     client.PipelineUpBuildCommand,
+		RedisSuffix:  client.RedisSuffixUpBuilt,
+		RedisMessage: module,
+	}, utils.EventedCommand{
+		LogMessage:   "Starting start command ...",
+		ExecLine:     client.PipelineUpStartCommand,
+		RedisSuffix:  client.RedisSuffixUpStarted,
+		RedisMessage: module,
+	}
+
+	pipeline := utils.Pipeline{
+		Module:                  module,
+		ModulePushedRedisSuffix: client.RedisSuffixUpPushed,
+		SrcDir:                  client.PipelineUpDirSrc,
+		PushDir:                 client.PipelineUpDirPush,
+		RunCommands:             []utils.EventedCommand{testCommand, buildCommand},
+		StartCommand:            startCommand,
+		StartCommandState:       commandStartState,
+		Git:                     git,
+		Redis:                   redis,
+	}
+
+	// Run the pipeline once. If there are errors, don't exit
+	if err := pipeline.RunAll(); err != nil {
+		log.Error("Error", rz.String("System", "Client"), rz.String("Module", module), rz.Err(err))
+	}
+
+	// Setup worker
+	pipelineUpdateWorker := &workers.PipelineUpdateWorker{
+		Modules:       downModules,
+		Pipeline:      pipeline,
+		LocalCloneDir: client.PipelineDownDirModules,
+		Redis:         redis,
+		RedisSuffix:   client.RedisSuffixUpPushed,
+		HTTPBaseURL:   client.GitUpBaseURL,
+	}
+
+	// Create channels
+	pipelineUpdateWorkerErrors := make(chan error, 0)
+	pipelineUpdateWorkerEvents := make(chan utils.Event, 0)
+
+	// Start worker
+	go pipelineUpdateWorker.Start(pipelineUpdateWorkerErrors, pipelineUpdateWorkerEvents)
+
+	// Create a new folder watcher
+	folderWatcher := utils.FolderWatcher{
+		WatchDir:    client.PipelineUpDirWatch,
+		IgnoreRegex: client.PipelineUpRegexIgnore,
+	}
+	folderWatcher.Start()
+
+	// Start the main loop
+	for folderWatcher.FolderWatcher.IsRunning() {
+		select {
+		// If there are errors, log the erros and exit
+		case err := <-pipelineUpdateWorkerErrors:
+			log.Fatal("Error", rz.String("System", "PipelineUpdateWorker"), rz.Err(err))
+		case event := <-pipelineUpdateWorkerEvents:
+			switch event.Code {
+			case 0:
+				log.Info("Started", rz.String("System", "PipelineUpdateWorker"), rz.String("EventMessage", event.Message))
+			case 1:
+				log.Info("Request", rz.String("System", "PipelineUpdateWorker"), rz.String("EventMessage", event.Message))
+			case 2:
+				log.Info("Stopped", rz.String("System", "PipelineUpdateWorker"), rz.String("EventMessage", event.Message))
+				return
+			default:
+				log.Fatal("Unknown event code", rz.String("System", "GitHTTPWorker"), rz.Int("EventCode", event.Code), rz.String("StatusMessage", event.Message))
+			}
+		case <-folderWatcher.FolderWatcher.ChangeDetails():
+			// Run the pipeline again on every file change. If there are errors, don't exit
+			if err := pipeline.RunAll(); err != nil {
+				log.Error("Error", rz.String("System", "Client"), rz.String("Module", module), rz.Err(err))
+			}
+		}
+	}
+
+}
