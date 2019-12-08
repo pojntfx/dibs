e1c79d99355fb6e516a79ff5a05b2915dca01dde
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-08 20:48:23 +0100

    build: Add ARM64 configuration

--- magefile.go
+++ magefile.go
@@ -47,6 +47,40 @@ var (
 				TestIntegrationDockerCommand:       "docker run --platform linux/amd64 pojntfx/godibs:linux-amd64 /usr/local/bin/godibs --help",
 				TestIntegrationDockerDockerContext: ".",
 				TestIntegrationDockerDockerfile:    "Dockerfile.testIntegrationDocker",
+			},
+			utils.BuildConfigV2{
+				Tag:      "pojntfx/godibs:linux-arm64",
+				Platform: "linux/arm64",
+
+				BinaryInContainerPath: "/usr/local/bin/godibs",
+				BinaryDistPath:        ".bin/godibs-linux-arm64",
+				CleanGlob:             ".bin/godibs-linux-arm64",
+
+				BuildCommand:       "go build -o .bin/godibs-linux-arm64 main.go",
+				BuildDockerContext: ".",
+				BuildDockerfile:    "Dockerfile",
+
+				BuildDockerTag:           "pojntfx/godibs-builddockerindocker:linux-arm64",
+				BuildDockerCommand:       "mage buildInDocker",
+				BuildDockerDockerContext: ".",
+				BuildDockerDockerfile:    "Dockerfile.docker",
+
+				TestUnitCommand:       "go test ./...",
+				TestUnitDockerContext: ".",
+				TestUnitDockerfile:    "Dockerfile.testUnit",
+
+				TestIntegrationGoCommand:       "go run main.go --help",
+				TestIntegrationGoDockerContext: ".",
+				TestIntegrationGoDockerfile:    "Dockerfile.testIntegrationGo",
+
+				TestIntegrationBinaryCommand:       ".bin/godibs-linux-arm64 --help",
+				TestIntegrationBinaryDockerContext: ".",
+				TestIntegrationBinaryDockerfile:    "Dockerfile.testIntegrationBinary",
+
+				TestIntegrationDockerTag:           "pojntfx/godibs-testintegrationdockerindocker:linux-arm64",
+				TestIntegrationDockerCommand:       "docker run --platform linux/arm64 pojntfx/godibs:linux-arm64 /usr/local/bin/godibs --help",
+				TestIntegrationDockerDockerContext: ".",
+				TestIntegrationDockerDockerfile:    "Dockerfile.testIntegrationDocker",
 			}}}
 )
 
