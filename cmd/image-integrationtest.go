69580759a2208285ac373de9c6dafad10f0225cb
Felix Pojtinger <felix@pojtinger.com>
2019-12-11 00:29:44 +0100

    feat: Add image integration test command

---
+++ cmd/image-integrationtest.go
@@ -0,0 +1,15 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var imageIntegrationtestCmd = &cobra.Command{
+	Use:   "integrationtest",
+	Short: "Run the integration tests",
+	Run: func(cmd *cobra.Command, args []string) {
+		buildConfigs.TestIntegrationDocker(PLATFORM)
+	},
+}
+
+func init() {
+	imageCmd.AddCommand(imageIntegrationtestCmd)
+}
