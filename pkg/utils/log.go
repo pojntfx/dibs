f56fbe925ed9e84a721d0fdc2f9221d003d02646
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-12 18:25:23 +0100

    feat: Remove need for code duplication by adding a universal `GetPlatforms` method to `Dibs`

---
+++ pkg/utils/log.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"gitlab.com/z0mbie42/rz-go/v2"
+	"gitlab.com/z0mbie42/rz-go/v2/log"
+)
+
+func PipeLogError(message string, err error, platform, output string) {
+	log.Fatal(message, rz.String("platform", platform), rz.String("output", output), rz.Err(err))
+}
+
+func PipeLogErrorPlatformNotFound(platform interface{}, err error) {
+	log.Fatal("Platform(s) not found in configuration file", rz.Any("platform", platform), rz.Err(err))
+}
