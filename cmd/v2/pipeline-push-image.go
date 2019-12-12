995121d956aaccf6e847cbb2f3fb48c7574877f8
Felix Pojtinger <felix@pojtinger.com>
2019-12-13 00:19:58 +0100

    feat: Add `push image` command

---
+++ cmd/v2/pipeline-push-image.go
@@ -0,0 +1,27 @@
+package v2
+
+import (
+	"github.com/pojntfx/godibs/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var PipelinePushImageCmd = &cobra.Command{
+	Use:   "image",
+	Short: "Push image",
+	Run: func(cmd *cobra.Command, args []string) {
+		platforms, err := Dibs.GetPlatforms(Platform, Platform == PlatformAll)
+		if err != nil {
+			utils.PipeLogErrorFatalPlatformNotFound(platforms, err)
+		}
+
+		for _, platform := range platforms {
+			if output, err := platform.Assets.Build.PushImage(platform.Platform); err != nil {
+				utils.PipeLogErrorFatal("Could not push image", err, platform.Platform, output)
+			}
+		}
+	},
+}
+
+func init() {
+	PipelinePushCmd.AddCommand(PipelinePushImageCmd)
+}
---
+++ cmd/v2/pipeline-push.go
@@ -0,0 +1,12 @@
+package v2
+
+import "github.com/spf13/cobra"
+
+var PipelinePushCmd = &cobra.Command{
+	Use:   "push",
+	Short: "Push with a pipeline building block",
+}
+
+func init() {
+	PipelineCmd.AddCommand(PipelinePushCmd)
+}
