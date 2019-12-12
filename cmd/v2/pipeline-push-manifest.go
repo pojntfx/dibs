35478bc20389f75a9b2eb7be64fb6114305705e8
Felix Pojtinger <felix@pojtinger.com>
2019-12-13 00:25:55 +0100

    feat: Add `push manifest` command

---
+++ cmd/v2/pipeline-push-manifest.go
@@ -0,0 +1,20 @@
+package v2
+
+import (
+	"github.com/pojntfx/godibs/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var PipelinePushManifestCmd = &cobra.Command{
+	Use:   "manifest",
+	Short: "Push manifest",
+	Run: func(cmd *cobra.Command, args []string) {
+		if output, err := Dibs.PushDockerManifest(Platform); err != nil {
+			utils.PipeLogErrorFatalNonPlatformSpecific("Could not push manifest", err, output)
+		}
+	},
+}
+
+func init() {
+	PipelinePushCmd.AddCommand(PipelinePushManifestCmd)
+}
