10462734fb6d06be72ec83fa09ff640d04496ef8
Felix Pojtinger <felix@pojtinger.com>
2019-12-12 13:24:39 +0100

    feat: Add manifest methods to dibs

--- pkg/utils/dibs.go
+++ pkg/utils/dibs.go
@@ -4,3 +4,17 @@ type Dibs struct {
 	Manifest  Manifest
 	Platforms []Platform
 }
+
+func (dibs *Dibs) BuildDockerManifest(imageTags []string) error {
+	for _, platform := range dibs.Platforms {
+		if err := platform.Binary.Build.execDocker("manifest", "create", "--amend", platform.Binary.Build.Tag, dibs.Manifest.Tag); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (dibs *Dibs) PushDockerManifest() error {
+	return dibs.Platforms[0].Binary.Build.execDocker("manifest", "push", dibs.Manifest.Tag)
+}
