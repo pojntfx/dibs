9c8c94acc2c17ac05de18925d182acc7655ca3e7
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-13 22:55:25 +0100

    feat: Add `integrationtest chart` command and corresponding Dockerfile.integrationTestChart

---
+++ cmd/pipeline-test-integration-chart.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/pojntfx/dibs/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var PipelineTestIntegrationChartCmd = &cobra.Command{
+	Use:   "chart",
+	Short: "Integration test the chart",
+	Run: func(cmd *cobra.Command, args []string) {
+		platforms, err := Dibs.GetPlatforms(Platform, Platform == PlatformAll)
+		if err != nil {
+			utils.PipeLogErrorFatalPlatformNotFound(platforms, err)
+		}
+
+		for _, platform := range platforms {
+			if Executor == ExecutorDocker {
+				if output, err := platform.Tests.Integration.Chart.BuildImage(platform.Platform); err != nil {
+					utils.PipeLogErrorFatal("Could not build chart integration test chart", err, platform.Platform, output)
+				}
+				output, err := platform.Tests.Integration.Chart.StartImage(platform.Platform)
+				utils.PipeLogErrorInfo("Chart integration test ran in Docker", err, platform.Platform, output)
+			} else {
+				output, err := platform.Tests.Integration.Chart.Start(platform.Platform)
+				utils.PipeLogErrorInfo("Chart integration test ran", err, platform.Platform, output)
+			}
+		}
+	},
+}
+
+func init() {
+	PipelineTestIntegrationCmd.AddCommand(PipelineTestIntegrationChartCmd)
+}
