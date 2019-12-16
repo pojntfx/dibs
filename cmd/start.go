69787ada5ae96d882644b2cc2e3cbcc534990bb4
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-17 00:31:04 +0100

    feat: Add `start` command

---
+++ cmd/start.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/pojntfx/dibs/pkg/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var StartCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start the project",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return ReadConfig(viper.GetString(DibsFileKey))
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		platformFromConfig := viper.GetString(PlatformKey)
+
+		platforms, err := Dibs.GetPlatforms(platformFromConfig, platformFromConfig == PlatformAll)
+		if err != nil {
+			utils.PipeLogErrorFatalPlatformNotFound(platforms, err)
+		}
+
+		for _, platform := range platforms {
+			if err := platform.Assets.Build.StartChart(platform.Platform, platform.ChartProfiles.Production); err != nil {
+				utils.PipeLogErrorFatalWithProfile("Could not start profile", err, platform.Platform, platform.ChartProfiles.Production)
+			}
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(StartCmd)
+}
