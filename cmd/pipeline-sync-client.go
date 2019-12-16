804e8109fd92b0feeeae2505cb052881e6941575
Felix Pojtinger <felix@pojtinger.com>
2019-12-16 12:11:27 +0100

    feat: Use viper for all sync command configuration, make flag pointers private

--- cmd/pipeline-sync-client.go
+++ cmd/pipeline-sync-client.go
@@ -16,7 +16,7 @@ var PipelineSyncClientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "Start the module development client",
 	Run: func(cmd *cobra.Command, args []string) {
-		switch Lang {
+		switch viper.GetString(LangKey) {
 		case LangGo:
 			// Ignore if there are errors here, platforms might not be set (there is no hard dependency on the config)
 			platforms, _ := Dibs.GetPlatforms(Platform, Platform == PlatformAll)
@@ -37,8 +37,8 @@ var PipelineSyncClientCmd = &cobra.Command{
 				PipelineUpDirWatch:     viper.GetString(PipelineUpDirWatchKey),
 				PipelineUpRegexIgnore:  strings.Replace(viper.GetString(PipelineUpRegexIgnoreKey), IgnoreRegexPlaceholder, ignoreRegex, -1),
 
-				RedisUrl:                  RedisUrl,
-				RedisPrefix:               RedisPrefix,
+				RedisUrl:                  viper.GetString(RedisUrlKey),
+				RedisPrefix:               viper.GetString(RedisPrefixKey),
 				RedisSuffixUpRegistered:   RedisSuffixUpRegistered,
 				RedisSuffixUpUnRegistered: RedisSuffixUpUnregistered,
 				RedisSuffixUpTested:       RedisSuffixUpTested,
@@ -58,40 +58,6 @@ var PipelineSyncClientCmd = &cobra.Command{
 	},
 }
 
-var (
-	GoGitUpBaseUrl string
-
-	PipelineUpDirSrc   string
-	PipelineUpDirPush  string
-	PipelineUpDirWatch string
-
-	GoPipelineUpFileMod string
-
-	PipelineUpBuildCommand string
-	PipelineUpTestCommand  string
-	PipelineUpStartCommand string
-
-	PipelineUpRegexIgnore    string
-	GoPipelineDownModules    string
-	GoPipelineDownDirModules string
-
-	GoGitBaseUrlFlag = strings.Replace(GoGitBaseUrlKey, "_", "-", -1)
-
-	PipelineUpDirSrcFlag   = strings.Replace(PipelineUpDirSrcKey, "_", "-", -1)
-	PipelineUpDirPushFlag  = strings.Replace(PipelineUpDirPushKey, "_", "-", -1)
-	PipelineUpDirWatchFlag = strings.Replace(PipelineUpDirWatchKey, "_", "-", -1)
-
-	GoPipelineUpFileModFlag = strings.Replace(GoPipelineUpFileModKey, "_", "-", -1)
-
-	PipelineUpBuildCommandFlag = strings.Replace(PipelineUpBuildCommandKey, "_", "-", -1)
-	PipelineUpTestCommandFlag  = strings.Replace(PipelineUpTestCommandKey, "_", "-", -1)
-	PipelineUpStartCommandFlag = strings.Replace(PipelineUpStartCommandKey, "_", "-", -1)
-
-	PipelineUpRegexIgnoreFlag    = strings.Replace(PipelineUpRegexIgnoreKey, "_", "-", -1)
-	GoPipelineDownModulesFlag    = strings.Replace(GoPipelineDownModulesKey, "_", "-", -1)
-	GoPipelineDownDirModulesFlag = strings.Replace(GoPipelineDownDirModulesKey, "_", "-", -1)
-)
-
 const (
 	GitUpCommitMessage = "up_synced"
 	GitUpRemoteName    = "dibs-sync"
@@ -100,8 +66,6 @@ const (
 
 	PlatformPlaceholder    = "[infer]"
 	IgnoreRegexPlaceholder = "[infer]"
-
-	EnvPrefix = "dibs"
 
 	GoGitBaseUrlKey = LangGo + "_git_base_url"
 
@@ -121,61 +85,95 @@ const (
 )
 
 func init() {
-	id := uuid.New().String()
+	var (
+		goGitUpBaseUrl string
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoGitUpBaseUrl, GoGitBaseUrlFlag, "http://localhost:35000/repos", `(--lang "`+LangGo+`" only) Base URL of the sync remote`)
+		pipelineUpDirSrc   string
+		pipelineUpDirPush  string
+		pipelineUpDirWatch string
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirSrc, PipelineUpDirSrcFlag, ".", "Directory in which the source code of the module to push resides")
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirPush, PipelineUpDirPushFlag, filepath.Join(os.TempDir(), "dibs", "push", id), "Temporary directory to put the module into before pushing")
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirWatch, PipelineUpDirWatchFlag, ".", "Directory to watch for changes")
+		goPipelineUpFileMod string
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoPipelineUpFileMod, GoPipelineUpFileModFlag, "go.mod", `(--lang "`+LangGo+`" only) Go module file of the module to push`)
+		pipelineUpBuildCommand string
+		pipelineUpTestCommand  string
+		pipelineUpStartCommand string
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpBuildCommand, PipelineUpBuildCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline build assets", "Command to run to build the module. Infers the platform from the parent command by default")
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpTestCommand, PipelineUpTestCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test unit lang", "Command to run to test the module. Infers the platform from the parent command by default")
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpStartCommand, PipelineUpStartCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test integration assets", "Command to run to start the module. Infers the platform from the parent command by default")
+		pipelineUpRegexIgnore    string
+		goPipelineDownModules    string
+		goPipelineDownDirModules string
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpRegexIgnore, PipelineUpRegexIgnoreFlag, IgnoreRegexPlaceholder, "Regular expression for files to ignore. If a dibs configuration file exists, it will infer it from assets.cleanGlob")
-	PipelineSyncClientCmd.PersistentFlags().StringVarP(&GoPipelineDownModules, GoPipelineDownModulesFlag, "g", "", `(--lang "`+LangGo+`" only) Comma-separated list of the names of the modules to pull`)
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoPipelineDownDirModules, GoPipelineDownDirModulesFlag, filepath.Join(os.TempDir(), "dibs", "pull", id), `(--lang "`+LangGo+`" only) Directory to pull the modules to`)
+		goGitBaseUrlFlag = strings.Replace(GoGitBaseUrlKey, "_", "-", -1)
+
+		pipelineUpDirSrcFlag   = strings.Replace(PipelineUpDirSrcKey, "_", "-", -1)
+		pipelineUpDirPushFlag  = strings.Replace(PipelineUpDirPushKey, "_", "-", -1)
+		pipelineUpDirWatchFlag = strings.Replace(PipelineUpDirWatchKey, "_", "-", -1)
+
+		goPipelineUpFileModFlag = strings.Replace(GoPipelineUpFileModKey, "_", "-", -1)
+
+		pipelineUpBuildCommandFlag = strings.Replace(PipelineUpBuildCommandKey, "_", "-", -1)
+		pipelineUpTestCommandFlag  = strings.Replace(PipelineUpTestCommandKey, "_", "-", -1)
+		pipelineUpStartCommandFlag = strings.Replace(PipelineUpStartCommandKey, "_", "-", -1)
+
+		pipelineUpRegexIgnoreFlag    = strings.Replace(PipelineUpRegexIgnoreKey, "_", "-", -1)
+		goPipelineDownModulesFlag    = strings.Replace(GoPipelineDownModulesKey, "_", "-", -1)
+		goPipelineDownDirModulesFlag = strings.Replace(GoPipelineDownDirModulesKey, "_", "-", -1)
+
+		id = uuid.New().String()
+	)
+
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&goGitUpBaseUrl, goGitBaseUrlFlag, "http://localhost:35000/repos", `(--lang "`+LangGo+`" only) Base URL of the sync remote`)
+
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpDirSrc, pipelineUpDirSrcFlag, ".", "Directory in which the source code of the module to push resides")
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpDirPush, pipelineUpDirPushFlag, filepath.Join(os.TempDir(), "dibs", "push", id), "Temporary directory to put the module into before pushing")
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpDirWatch, pipelineUpDirWatchFlag, ".", "Directory to watch for changes")
+
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&goPipelineUpFileMod, goPipelineUpFileModFlag, "go.mod", `(--lang "`+LangGo+`" only) Go module file of the module to push`)
+
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpBuildCommand, pipelineUpBuildCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline build assets", "Command to run to build the module. Infers the platform from the parent command by default")
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpTestCommand, pipelineUpTestCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test unit lang", "Command to run to test the module. Infers the platform from the parent command by default")
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpStartCommand, pipelineUpStartCommandFlag, os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test integration assets", "Command to run to start the module. Infers the platform from the parent command by default")
+
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&pipelineUpRegexIgnore, pipelineUpRegexIgnoreFlag, IgnoreRegexPlaceholder, "Regular expression for files to ignore. If a dibs configuration file exists, it will infer it from assets.cleanGlob")
+	PipelineSyncClientCmd.PersistentFlags().StringVarP(&goPipelineDownModules, goPipelineDownModulesFlag, "g", "", `(--lang "`+LangGo+`" only) Comma-separated list of the names of the modules to pull`)
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&goPipelineDownDirModules, goPipelineDownDirModulesFlag, filepath.Join(os.TempDir(), "dibs", "pull", id), `(--lang "`+LangGo+`" only) Directory to pull the modules to`)
 
 	viper.SetEnvPrefix(EnvPrefix)
 
-	if err := viper.BindPFlag(GoGitBaseUrlKey, PipelineSyncClientCmd.PersistentFlags().Lookup(GoGitBaseUrlFlag)); err != nil {
+	if err := viper.BindPFlag(GoGitBaseUrlKey, PipelineSyncClientCmd.PersistentFlags().Lookup(goGitBaseUrlFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
 
-	if err := viper.BindPFlag(PipelineUpDirSrcKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpDirSrcFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpDirSrcKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpDirSrcFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
-	if err := viper.BindPFlag(PipelineUpDirPushKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpDirPushFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpDirPushKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpDirPushFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
-	if err := viper.BindPFlag(PipelineUpDirWatchKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpDirWatchFlag)); err != nil {
-		log.Fatal("Could not bind flag", rz.Err(err))
-	}
-
-	if err := viper.BindPFlag(GoPipelineUpFileModKey, PipelineSyncClientCmd.PersistentFlags().Lookup(GoPipelineUpFileModFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpDirWatchKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpDirWatchFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
 
-	if err := viper.BindPFlag(PipelineUpBuildCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpBuildCommandFlag)); err != nil {
-		log.Fatal("Could not bind flag", rz.Err(err))
-	}
-	if err := viper.BindPFlag(PipelineUpTestCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpTestCommandFlag)); err != nil {
-		log.Fatal("Could not bind flag", rz.Err(err))
-	}
-	if err := viper.BindPFlag(PipelineUpStartCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpStartCommandFlag)); err != nil {
+	if err := viper.BindPFlag(GoPipelineUpFileModKey, PipelineSyncClientCmd.PersistentFlags().Lookup(goPipelineUpFileModFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
 
-	if err := viper.BindPFlag(PipelineUpRegexIgnoreKey, PipelineSyncClientCmd.PersistentFlags().Lookup(PipelineUpRegexIgnoreFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpBuildCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpBuildCommandFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
-	if err := viper.BindPFlag(GoPipelineDownModulesKey, PipelineSyncClientCmd.PersistentFlags().Lookup(GoPipelineDownModulesFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpTestCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpTestCommandFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
-	if err := viper.BindPFlag(GoPipelineDownDirModulesKey, PipelineSyncClientCmd.PersistentFlags().Lookup(GoPipelineDownDirModulesFlag)); err != nil {
+	if err := viper.BindPFlag(PipelineUpStartCommandKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpStartCommandFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+
+	if err := viper.BindPFlag(PipelineUpRegexIgnoreKey, PipelineSyncClientCmd.PersistentFlags().Lookup(pipelineUpRegexIgnoreFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+	if err := viper.BindPFlag(GoPipelineDownModulesKey, PipelineSyncClientCmd.PersistentFlags().Lookup(goPipelineDownModulesFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+	if err := viper.BindPFlag(GoPipelineDownDirModulesKey, PipelineSyncClientCmd.PersistentFlags().Lookup(goPipelineDownDirModulesFlag)); err != nil {
 		log.Fatal("Could not bind flag", rz.Err(err))
 	}
 
--- cmd/pipeline-sync.go
+++ cmd/pipeline-sync.go
@@ -3,23 +3,25 @@ package cmd
 import (
 	"errors"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gitlab.com/z0mbie42/rz-go/v2"
+	"gitlab.com/z0mbie42/rz-go/v2/log"
+	"strings"
 )
 
 var PipelineSyncCmd = &cobra.Command{
 	Use:   "sync",
 	Short: "Sync with a pipeline building block",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		if !(Lang == LangGo) {
-			return errors.New(`unsupported value "` + Lang + `" for --lang, must be "` + LangGo + `"`)
+		lang := viper.GetString(LangKey)
+
+		if !(lang == LangGo) {
+			return errors.New(`unsupported value "` + lang + `" for --lang, must be "` + LangGo + `"`)
 		}
 
 		return nil
 	},
 }
-
-var (
-	Lang string
-)
 
 const (
 	LangDefault = LangGo
@@ -27,13 +29,45 @@ const (
 
 	RedisUrlDefault    = "localhost:6379"
 	RedisPrefixDefault = "dibs"
+
+	LangKey = "lang"
+
+	RedisUrlKey    = "redis_url"
+	RedisPrefixKey = "redis_prefix"
 )
 
 func init() {
-	PipelineSyncCmd.PersistentFlags().StringVarP(&Lang, "lang", "l", LangDefault, `Language to develop the modules for (currently only "`+LangGo+`" is supported)`)
+	var (
+		lang string
 
-	PipelineSyncCmd.PersistentFlags().StringVarP(&RedisUrl, "redis-url", "u", RedisUrlDefault, "URL of the Redis instance to use")
-	PipelineSyncCmd.PersistentFlags().StringVarP(&RedisPrefix, "redis-prefix", "c", RedisPrefixDefault, "Redis channel prefix to use")
+		redisUrl    string
+		redisPrefix string
+
+		langFlag = strings.Replace(LangKey, "_", "-", -1)
+
+		redisUrlFlag    = strings.Replace(RedisUrlKey, "_", "-", -1)
+		redisPrefixFlag = strings.Replace(RedisPrefixKey, "_", "-", -1)
+	)
+
+	PipelineSyncCmd.PersistentFlags().StringVarP(&lang, langFlag, "l", LangDefault, `Language to develop the modules for (currently only "`+LangGo+`" is supported)`)
+
+	PipelineSyncCmd.PersistentFlags().StringVarP(&redisUrl, redisUrlFlag, "u", RedisUrlDefault, "URL of the Redis instance to use")
+	PipelineSyncCmd.PersistentFlags().StringVarP(&redisPrefix, redisPrefixFlag, "c", RedisPrefixDefault, "Redis channel prefix to use")
+
+	viper.SetEnvPrefix(EnvPrefix)
+
+	if err := viper.BindPFlag(LangKey, PipelineSyncClientCmd.PersistentFlags().Lookup(langFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+
+	if err := viper.BindPFlag(RedisUrlKey, PipelineSyncClientCmd.PersistentFlags().Lookup(redisUrlFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+	if err := viper.BindPFlag(RedisPrefixKey, PipelineSyncClientCmd.PersistentFlags().Lookup(redisPrefixFlag)); err != nil {
+		log.Fatal("Could not bind flag", rz.Err(err))
+	}
+
+	viper.AutomaticEnv()
 
 	PipelineCmd.AddCommand(PipelineSyncCmd)
 }
