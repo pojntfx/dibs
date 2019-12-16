5609ceaf59e5d3b73bb2dba663f048f266bbd677
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-16 10:58:15 +0100

    feat: Start adding env configuration capabilities to sync client

--- cmd/pipeline-sync-client.go
+++ cmd/pipeline-sync-client.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/pojntfx/dibs/pkg/starters"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
 	"strings"
@@ -23,9 +24,9 @@ var PipelineSyncClientCmd = &cobra.Command{
 			}
 
 			client := starters.Client{
-				PipelineUpFileMod:      PipelineUpFileMod,
-				PipelineDownModules:    PipelineDownModules,
-				PipelineDownDirModules: PipelineDownDirModules,
+				PipelineUpFileMod:      GoPipelineUpFileMod,
+				PipelineDownModules:    GoPipelineDownModules,
+				PipelineDownDirModules: GoPipelineDownDirModules,
 				PipelineUpBuildCommand: strings.Replace(PipelineUpBuildCommand, PlatformPlaceholder, Platform, -1),
 				PipelineUpStartCommand: strings.Replace(PipelineUpStartCommand, PlatformPlaceholder, Platform, -1),
 				PipelineUpTestCommand:  strings.Replace(PipelineUpTestCommand, PlatformPlaceholder, Platform, -1),
@@ -44,7 +45,7 @@ var PipelineSyncClientCmd = &cobra.Command{
 				RedisSuffixUpPushed:       RedisSuffixUpPushed,
 
 				GitUpRemoteName:    GitUpRemoteName,
-				GitUpBaseURL:       GitUpBaseUrl,
+				GitUpBaseURL:       viper.GetString(GoGitBaseUrlKey),
 				GitUpUserName:      GitUpUserName,
 				GitUpUserEmail:     GitUpUserEmail,
 				GitUpCommitMessage: GitUpCommitMessage,
@@ -56,19 +57,19 @@ var PipelineSyncClientCmd = &cobra.Command{
 }
 
 var (
-	GitUpBaseUrl string
+	GoGitUpBaseUrl string
 
 	PipelineUpDirSrc       string
 	PipelineUpDirPush      string
 	PipelineUpDirWatch     string
-	PipelineUpFileMod      string
+	GoPipelineUpFileMod    string
 	PipelineUpBuildCommand string
 	PipelineUpTestCommand  string
 	PipelineUpStartCommand string
 	PipelineUpRegexIgnore  string
 
-	PipelineDownModules    string
-	PipelineDownDirModules string
+	GoPipelineDownModules    string
+	GoPipelineDownDirModules string
 )
 
 const (
@@ -79,23 +80,34 @@ const (
 
 	PlatformPlaceholder    = "[infer]"
 	IgnoreRegexPlaceholder = "[infer]"
+
+	EnvPrefix = "dibs"
+
+	GoGitBaseUrlFlag = LangGo + "-git-base-url"
+	GoGitBaseUrlKey  = LangGo + "_git_base_url"
 )
 
 func init() {
 	id := uuid.New().String()
 
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&GitUpBaseUrl, LangGo+"-git-base-url", "http://localhost:35000/repos", `(--lang "`+LangGo+`" only) Base URL of the sync remote`)
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoGitUpBaseUrl, GoGitBaseUrlFlag, "http://localhost:35000/repos", `(--lang "`+LangGo+`" only) Base URL of the sync remote`)
 
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirSrc, "dir-src", ".", "Directory in which the source code of the module to push resides")
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirPush, "dir-push", filepath.Join(os.TempDir(), "dibs", "push", id), "Temporary directory to put the module into before pushing")
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpDirWatch, "dir-watch", ".", "Directory to watch for changes")
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpFileMod, LangGo+"-modules-file", "go.mod", `(--lang "`+LangGo+`" only) Go module file of the module to push`)
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoPipelineUpFileMod, LangGo+"-modules-file", "go.mod", `(--lang "`+LangGo+`" only) Go module file of the module to push`)
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpBuildCommand, "cmd-build", os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline build assets", "Command to run to build the module. Infers the platform from the parent command by default")
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpTestCommand, "cmd-test", os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test unit lang", "Command to run to test the module. Infers the platform from the parent command by default")
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpStartCommand, "cmd-start", os.Args[0]+" --platform "+PlatformPlaceholder+" pipeline test integration assets", "Command to run to start the module. Infers the platform from the parent command by default")
 	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineUpRegexIgnore, "regex-ignore", IgnoreRegexPlaceholder, "Regular expression for files to ignore. If a dibs configuration file exists, it will infer it from assets.cleanGlob")
-	PipelineSyncClientCmd.PersistentFlags().StringVarP(&PipelineDownModules, LangGo+"-modules-pull", "g", "", `(--lang "`+LangGo+`" only) Comma-separated list of the names of the modules to pull`)
-	PipelineSyncClientCmd.PersistentFlags().StringVar(&PipelineDownDirModules, LangGo+"-dir-pull", filepath.Join(os.TempDir(), "dibs", "pull", id), `(--lang "`+LangGo+`" only) Directory to pull the modules to`)
+	PipelineSyncClientCmd.PersistentFlags().StringVarP(&GoPipelineDownModules, LangGo+"-modules-pull", "g", "", `(--lang "`+LangGo+`" only) Comma-separated list of the names of the modules to pull`)
+	PipelineSyncClientCmd.PersistentFlags().StringVar(&GoPipelineDownDirModules, LangGo+"-dir-pull", filepath.Join(os.TempDir(), "dibs", "pull", id), `(--lang "`+LangGo+`" only) Directory to pull the modules to`)
+
+	viper.SetEnvPrefix(EnvPrefix)
+
+	viper.BindPFlag(GoGitBaseUrlKey, PipelineSyncClientCmd.PersistentFlags().Lookup(GoGitBaseUrlFlag))
+
+	viper.AutomaticEnv()
 
 	PipelineSyncCmd.AddCommand(PipelineSyncClientCmd)
 }
