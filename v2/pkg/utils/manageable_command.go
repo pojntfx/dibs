11e0ffb85e193f5675e13feed534e1fa6aaaf7f6
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-02-26 20:17:58 +0100

    feat: Add helper for a manageable command

---
+++ v2/pkg/utils/manageable_command.go
@@ -0,0 +1,88 @@
+package utils
+
+import (
+	"bufio"
+	"io"
+	"os/exec"
+	"syscall"
+)
+
+// ManageableCommand is a manageable command
+type ManageableCommand interface {
+	Start() error
+	Wait() error
+	Stop() error
+	Restart() error
+}
+
+// ManageableCommandImpl is an implementation of ManageableCommand
+type ManageableCommandImpl struct {
+	execLine               string
+	stdoutChan, stderrChan chan string
+	instance               *exec.Cmd
+}
+
+func readFromReader(reader io.Reader, outChan chan string) {
+	bufStdout := bufio.NewReader(reader)
+
+	for {
+		line, _, err := bufStdout.ReadLine()
+		if err != nil {
+			return
+		}
+
+		outChan <- string(line)
+	}
+}
+
+func getCommandWrappedInSh(execLine string) *exec.Cmd {
+	wrappedArgs := append([]string{"sh", "-c"}, execLine)
+
+	return exec.Command(wrappedArgs[0], wrappedArgs[1:]...)
+}
+
+// Start starts the command
+func (r *ManageableCommandImpl) Start() error {
+	r.instance = getCommandWrappedInSh(r.execLine)
+
+	stdout, err := r.instance.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := r.instance.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	go readFromReader(stdout, r.stdoutChan)
+	go readFromReader(stderr, r.stderrChan)
+
+	r.instance.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	return r.instance.Start()
+}
+
+// Wait waits for the command to complete
+func (r *ManageableCommandImpl) Wait() error { return r.instance.Wait() }
+
+// Stop stops the command
+func (r *ManageableCommandImpl) Stop() error {
+	processGroupID, err := syscall.Getpgid(r.instance.Process.Pid)
+	if err != nil {
+		return err
+	}
+
+	return syscall.Kill(processGroupID, syscall.SIGKILL)
+}
+
+// Restart restarts the command
+func (r *ManageableCommandImpl) Restart() error { return nil }
+
+// New creates a new ManageableCommand
+func New(execLine string, stdoutChan chan string, stderrChan chan string) *ManageableCommandImpl {
+	return &ManageableCommandImpl{
+		execLine:   execLine,
+		stdoutChan: stdoutChan,
+		stderrChan: stderrChan,
+	}
+}
