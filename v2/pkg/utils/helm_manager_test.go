ceacb34cff242d37dc50becdf340c5a3e5f73a57
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-03-03 21:20:20 +0100

    feat: Add basic Helm chart pusher

--- v2/pkg/utils/helm_manager_test.go
+++ v2/pkg/utils/helm_manager_test.go
@@ -9,6 +9,7 @@ import (
 var (
 	testHelmChartSrc  = filepath.Join(testContext, "charts", "test-app")
 	testHelmChartDist = filepath.Join(os.TempDir(), "test-app-charts")
+	testCloneDir      = filepath.Join(os.TempDir(), "test-clone-dir")
 )
 
 func TestCreateHelmManager(t *testing.T) {
@@ -68,5 +69,52 @@ func TestBuildHelmManager(t *testing.T) {
 
 	if len(matches) == 0 {
 		t.Error("Built Helm chart could not be found")
+	}
+}
+
+// TestPushHelmManager requires the environment variables below to be set; it is disabled by default.
+func TestPushHelmManager(t *testing.T) {
+	if os.Getenv("DIBS_PUSH_TEST_ENABLED") == "1" {
+		stdoutChan, stderrChan := make(chan string), make(chan string)
+
+		h := NewHelmManager(testContext, stdoutChan, stderrChan)
+
+		if err := os.RemoveAll(testHelmChartDist); err != nil {
+			t.Error(err)
+		}
+
+		if err := os.MkdirAll(testHelmChartDist, 0777); err != nil {
+			t.Error(err)
+		}
+
+		go func() {
+			for {
+				select {
+				case stdout := <-stdoutChan:
+					t.Log("test stdout", stdout)
+				case stderr := <-stderrChan:
+					t.Error("error while building Helm chart", stderr)
+				}
+			}
+		}()
+
+		if err := h.Build(testHelmChartSrc, testHelmChartDist); err != nil {
+			t.Error(err)
+		}
+
+		if err := h.Push(
+			os.Getenv("DIBS_GIT_USER_NAME"),
+			os.Getenv("DIBS_GIT_USER_EMAIL"),
+			os.Getenv("DIBS_GIT_COMMIT_MESSAGE"),
+			os.Getenv("DIBS_GITHUB_USER_NAME"),
+			os.Getenv("DIBS_GITHUB_TOKEN"),
+			os.Getenv("DIBS_GITHUB_REPOSITORY_NAME"),
+			os.Getenv("DIBS_GITHUB_REPOSITORY_URL"),
+			os.Getenv("DIBS_GITHUB_PAGES_URL"),
+			testHelmChartDist,
+			testCloneDir,
+		); err != nil {
+			t.Error(err)
+		}
 	}
 }
