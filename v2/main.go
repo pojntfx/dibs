5035dc811ab0a2803ee9acbadc0d34b208b005a3
Felix Pojtinger <felix@pojtinger.com>
2020-03-03 11:19:29 +0100

    feat: Add basic CLI capabilities

--- v2/main.go
+++ v2/main.go
@@ -23,32 +23,10 @@ type Config struct {
 	}
 }
 
-func main() {
-	var configFilePath string
-	flag.StringVar(&configFilePath, "config-file", "dibs.yaml", "The config file to use")
-	flag.Parse()
+func runCommandWithLog(execLine string, stdoutChan, stderrChan chan string) {
+	command := utils.NewManageableCommand(execLine, stdoutChan, stderrChan)
 
-	configFile, err := ioutil.ReadFile(configFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	config := Config{}
-	if err := yaml.Unmarshal(configFile, &config); err != nil {
-		log.Fatal(err)
-	}
-
-	stdoutChan, stderrChan := make(chan string), make(chan string)
-
-	commandFlow := utils.NewCommandFlow([]string{
-		config.Commands.GenerateSources,
-		config.Commands.Build,
-		config.Commands.UnitTests,
-		config.Commands.IntegrationTests,
-		config.Commands.Start,
-	}, stdoutChan, stderrChan)
-
-	if err := commandFlow.Start(); err != nil {
+	if err := command.Start(); err != nil {
 		log.Fatal(err)
 	}
 
@@ -63,27 +41,105 @@ func main() {
 		}
 	}()
 
-	eventChan := make(chan string)
-
-	pathWatcher := utils.NewPathWatcher(config.Paths.Watch, config.Paths.Include, eventChan)
-
-	go func() {
-		for {
-			select {
-			case <-eventChan:
-				if err := commandFlow.Restart(); err != nil {
-					log.Fatal(err)
-				}
-			}
+	if err := command.Wait(); err != nil {
+		if err.Error() == "exit status 2" { // -help
+			return
 		}
-	}()
+		log.Fatal(err)
+	}
+}
 
-	defer func() {
-		if err := commandFlow.Stop(); err != nil {
+func main() {
+	var (
+		configFilePath   string
+		dev              bool
+		generateSources  bool
+		build            bool
+		unitTests        bool
+		integrationTests bool
+	)
+	flag.StringVar(&configFilePath, "configFile", "dibs.yaml", "The config file to use")
+	flag.BoolVar(&dev, "dev", false, "Run the development mode for the project")
+	flag.BoolVar(&generateSources, "generateSources", false, "Generate the sources for the project")
+	flag.BoolVar(&build, "build", false, "Build the project")
+	flag.BoolVar(&unitTests, "unitTests", false, "Run the unit tests to the project")
+	flag.BoolVar(&integrationTests, "integrationTests", false, "Run the integration tests to the project")
+	flag.Parse()
+
+	configFile, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := Config{}
+	if err := yaml.Unmarshal(configFile, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	stdoutChan, stderrChan := make(chan string), make(chan string)
+
+	if dev {
+		commandFlow := utils.NewCommandFlow([]string{
+			config.Commands.GenerateSources,
+			config.Commands.Build,
+			config.Commands.UnitTests,
+			config.Commands.IntegrationTests,
+			config.Commands.Start,
+		}, stdoutChan, stderrChan)
+
+		if err := commandFlow.Start(); err != nil {
 			log.Fatal(err)
 		}
-	}()
-	if err := pathWatcher.Start(); err != nil {
-		log.Fatal(err)
+
+		go func() {
+			for {
+				select {
+				case stdout := <-stdoutChan:
+					log.Println("STDOUT", stdout)
+				case stderr := <-stderrChan:
+					log.Println("STDERR", stderr)
+				}
+			}
+		}()
+
+		eventChan := make(chan string)
+
+		pathWatcher := utils.NewPathWatcher(config.Paths.Watch, config.Paths.Include, eventChan)
+
+		go func() {
+			for {
+				select {
+				case <-eventChan:
+					if err := commandFlow.Restart(); err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+		}()
+
+		defer func() {
+			if err := commandFlow.Stop(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+		if err := pathWatcher.Start(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if generateSources {
+		runCommandWithLog(config.Commands.GenerateSources, stdoutChan, stderrChan)
+	}
+
+	if build {
+		runCommandWithLog(config.Commands.Build, stdoutChan, stderrChan)
+	}
+
+	if unitTests {
+		runCommandWithLog(config.Commands.UnitTests, stdoutChan, stderrChan)
+	}
+
+	if integrationTests {
+		runCommandWithLog(config.Commands.IntegrationTests, stdoutChan, stderrChan)
 	}
 }
