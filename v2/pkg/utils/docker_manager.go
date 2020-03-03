224b2f55e20086c79f16783ee64548c1b69daa4c
Felix Pojtinger <felix@pojtinger.com>
2020-03-03 14:40:21 +0100

    feat: Add `-buildImage` action

--- v2/pkg/utils/docker_manager.go
+++ v2/pkg/utils/docker_manager.go
@@ -2,9 +2,24 @@ package utils
 
 // DockerManager manages Docker
 type DockerManager struct {
+	stdoutChan, stderrChan chan string
 }
 
 // NewDockerManager creates a new DockerManager
-func NewDockerManager() *DockerManager {
-	return &DockerManager{}
+func NewDockerManager(stdoutChan, stderrChan chan string) *DockerManager {
+	return &DockerManager{
+		stdoutChan: stdoutChan,
+		stderrChan: stderrChan,
+	}
+}
+
+// Build builds and tags a Docker image
+func (d *DockerManager) Build(file, context, tag string) error {
+	command := NewManageableCommand("docker build -f "+file+" -t "+tag+" "+context, d.stdoutChan, d.stderrChan)
+
+	if err := command.Start(); err != nil {
+		return err
+	}
+
+	return command.Wait()
 }
--- v2/pkg/utils/docker_manager_test.go
+++ v2/pkg/utils/docker_manager_test.go
@@ -1,11 +1,61 @@
 package utils
 
-import "testing"
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+var (
+	testContext    = filepath.Join("..", "..", "test-app")
+	testDockerfile = filepath.Join(testContext, "Dockerfile")
+	testTag        = filepath.Join("pojntfx/test-app")
+)
 
 func TestCreateDockerManager(t *testing.T) {
-	d := NewDockerManager()
+	stdoutChan, stderrChan := make(chan string), make(chan string)
+
+	d := NewDockerManager(stdoutChan, stderrChan)
 
 	if d == nil {
 		t.Error("New Docker manager is nil")
+	}
+
+	if d.stdoutChan != stdoutChan {
+		t.Error("stdoutChan not set correctly")
+	}
+
+	if d.stderrChan != stderrChan {
+		t.Error("stderrChan not correctly")
+	}
+}
+
+func TestBuildDockerManager(t *testing.T) {
+	stdoutChan, stderrChan := make(chan string), make(chan string)
+
+	d := NewDockerManager(stdoutChan, stderrChan)
+
+	hits := 0
+	go func() {
+		for {
+			select {
+			case stdout := <-stdoutChan:
+				t.Log("test stdout", stdout)
+
+				if strings.Contains(stdout, "Successfully built") || strings.Contains(stdout, "Successfully tagged") {
+					hits++
+				}
+			case stderr := <-stderrChan:
+				t.Error("error while building Docker image", stderr)
+			}
+		}
+	}()
+
+	if err := d.Build(testDockerfile, testContext, testTag); err != nil {
+		t.Error(err)
+	}
+
+	if hits < 2 {
+		t.Error("Docker output did not match expected output")
 	}
 }
